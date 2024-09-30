18de5c890ee91173ba411804f9b8487194c24b2b
Laiyu Pei （裴来宇） <2797146538pei@gmail.com>
2024-09-30 13:04:10 +1300

    feat: Styles

--- src/components/Editor/types.ts
+++ src/components/Editor/types.ts
@@ -20,25 +20,25 @@ export const rootTypeToRootName = {
 
 
 // https://tailwindcss.com/docs/customizing-colors
-// 全部选取 500
+// 全部选取 300
 export const colorTypes = {
 	Black: '#000000',
 	White: '#ffffff',
-	Gray: '#6b7280',
-	Red: '#ef4444',
-	Orange: '#f97316',
-	Amber: '#f59e0b',
-	Yellow: '#eab308',
-	Lime: '#84cc16',
-	Green: '#22c55e',
-	Emerald: '#10b981',
-	Teal: '#14b8a6',
-	Cyan: '#06b6d4',
-	Sky: '#0ea5e9',
-	Blue: '#3b82f6',
-	Indigo: '#6366f1',
-	Violet: '#8b5cf6',
-	Purple: '#a855f7',
-	Fuchsia: '#d946ef',
-	Pink: '#ec4899',
+	Gray: '#d1d5db',  // Gray-300
+	Red: '#fca5a5',   // Red-300
+	Orange: '#fdba74', // Orange-300
+	Amber: '#fcd34d',  // Amber-300
+	Yellow: '#fde047', // Yellow-300
+	Lime: '#bef264',   // Lime-300
+	Green: '#86efac',  // Green-300
+	Emerald: '#6ee7b7', // Emerald-300
+	Teal: '#5eead4',   // Teal-300
+	Cyan: '#67e8f9',   // Cyan-300
+	Sky: '#7dd3fc',    // Sky-300
+	Blue: '#93c5fd',   // Blue-300
+	Indigo: '#a5b4fc', // Indigo-300
+	Violet: '#c4b5fd', // Violet-300
+	Purple: '#d8b4fe', // Purple-300
+	Fuchsia: '#f0abfc', // Fuchsia-300
+	Pink: '#f9a8d4',   // Pink-300
 };
